5f330b8adadbdf97a1d444507634623324e64562
Fernando Machado <lfmachado90@gmail.com>
2020-10-07 01:28:29 -0300

    updates missing test

--- src/Book.test.js
+++ src/Book.test.js
@@ -6,5 +6,6 @@ test("renders Book box", () => {
   const { getByText } = render(<Book className="Box Test" />)
 
   const book = getByText("book")
+  expect(book).toBeInTheDocument()
   expect(book).toHaveClass("Book", "Box", "Test")
 })
