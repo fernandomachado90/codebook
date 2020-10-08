bcd78029fdf888cdb52de66449db518b0d56f857
Fernando Machado <lfmachado90@gmail.com>
2020-10-08 03:26:40 -0300

    Allows Book to receive and render header and contents props

--- src/Book.js
+++ src/Book.js
@@ -1,8 +1,16 @@
 import React from "react"
 import "./Book.css"
 
-function Book({ className }) {
-  return <div className={`Book ${className ? className : ""}`}>book</div>
+function Book({ header, content, className }) {
+  return (
+    <div className={`Book ${className ? className : ""}`}>
+      <div className="Header">{header}</div>
+      <div className="Content">{content}</div>
+      <div className="Footer">
+        © Janela Web /<a href="https://github.com/fernandomachado90/janela-web">Contribute</a>
+      </div>
+    </div>
+  )
 }
 
 export default Book
--- src/Book.test.js
+++ src/Book.test.js
@@ -3,9 +3,21 @@ import { render } from "@testing-library/react"
 import Book from "./Book"
 
 test("renders Book box", () => {
-  const { getByText } = render(<Book className="Box Test" />)
+  const title = "Testing"
+  const text = "Lorem Ipsum"
 
-  const book = getByText("book")
+  const { container, getByText } = render(<Book header={title} content={text} className="Box Test" />)
+
+  const book = container.firstChild
   expect(book).toBeInTheDocument()
   expect(book).toHaveClass("Book", "Box", "Test")
+
+  const header = getByText(title)
+  expect(header).toHaveClass("Header")
+
+  const content = getByText(text)
+  expect(content).toHaveClass("Content")
+
+  const footer = getByText(/Janela Web/i)
+  expect(footer).toHaveClass("Footer")
 })
