3ddd1ea8783a60c12a26a731bab89aa02bf6eca7
Fernando Machado <lfmachado90@gmail.com>
2021-05-13 03:13:40 -0300

    updates pages contents

--- src/pages/3.js
+++ src/pages/3.js
@@ -2,9 +2,12 @@ import React from "react"
 
 export const body = (
   <div>
-    <h1>My Third Heading</h1>
-    <p>He he hee.</p>
+    <h1>hello</h1>
+    <h2>turn</h2>
+    <h3>your</h3>
+    <h4>radio</h4>
+    <h5>on</h5>
+    <h6>!</h6>
   </div>
 )
-
-export const title = "..."
+export const title = "Third page"
