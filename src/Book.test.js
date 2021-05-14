2eee9600164def0c3fa645922d58939b4920d6a6
Fernando Machado <lfmachado90@gmail.com>
2021-05-14 04:12:22 -0300

    includes page navigation tests

--- src/Book.test.js
+++ src/Book.test.js
@@ -1,21 +1,28 @@
 import React from "react"
-import { render } from "@testing-library/react"
+import { render, act } from "@testing-library/react"
 import Book from "./Book"
 
 jest.mock("./pages/0.js", () => {
   return {
-    title: "Test Title",
-    body: <>Test body</>,
+    title: "title zero",
+    body: <>body zero</>,
+  }
+})
+
+jest.mock("./pages/1.js", () => {
+  return {
+    title: "title one",
+    body: <>body one</>,
   }
 })
 
 test("renders Book box", async () => {
   const { container, getByText, findByText } = render(<Book className="Box Test" />)
 
-  const header = await findByText(/Test Title/i)
+  const header = await findByText(/title zero/i)
   expect(header).toHaveClass("Header")
 
-  const content = await findByText(/Test body/i)
+  const content = await findByText(/body zero/i)
   expect(content).toHaveClass("Content")
 
   const footer = getByText(/2020/i)
@@ -24,4 +31,22 @@ test("renders Book box", async () => {
   const book = container.firstChild
   expect(book).toBeInTheDocument()
   expect(book).toHaveClass("Book", "Box", "Test")
+})
+
+test("Book pages navigation buttons", async () => {
+  const { findByText } = render(<Book className="Box Test" />)
+
+  const next = await findByText(">")
+  await act(async () => {
+    next.click()
+  })
+  expect(await findByText(/title one/i)).toBeInTheDocument()
+  expect(await findByText(/body one/i)).toBeInTheDocument()
+
+  const previous = await findByText("<")
+  await act(async () => {
+    previous.click()
+  })
+  expect(await findByText(/title zero/i)).toBeInTheDocument()
+  expect(await findByText(/body zero/i)).toBeInTheDocument()
 })
