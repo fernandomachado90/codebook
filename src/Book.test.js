49f8543422d615ae2e041062e64313c5a0bc64d1
Fernando Machado <lfmachado90@gmail.com>
2021-06-22 04:14:58 -0300

    makes test book pages mock virtual

--- src/Book.test.js
+++ src/Book.test.js
@@ -2,37 +2,39 @@ import React from "react"
 import { render, act } from "@testing-library/react"
 import Book from "./Book"
 
-jest.mock("./BookAPI", () => {
-  return {
-    fetchBookPages: () => ["./book/0", "./book/1"],
-    selectRandomTheme: () => "Test",
-  }
-})
+jest.mock("./BookAPI", () => ({
+  fetchBookPages: () => ["./testbook/00-start", "./testbook/99-end"],
+  selectRandomTheme: () => "Test",
+}))
 
-jest.mock("./book/0", () => {
-  return {
-    title: "title zero",
-    body: <>body zero</>,
-  }
-})
+jest.mock(
+  "./testbook/00-start",
+  () => ({
+    title: "hello",
+    body: <>this is a test</>,
+  }),
+  { virtual: true }
+)
 
-jest.mock("./book/1", () => {
-  return {
-    title: "title one",
-    body: <>body one</>,
-  }
-})
+jest.mock(
+  "./testbook/99-end",
+  () => ({
+    title: "goodbye",
+    body: <>hope all went well</>,
+  }),
+  { virtual: true }
+)
 
 test("renders Book box", async () => {
   const { container, getByText, findByText } = render(<Book className="Box Test" />)
 
-  const header = await findByText(/title zero/i)
+  const header = await findByText(/hello/i)
   expect(header.parentElement).toHaveClass("Header")
 
-  const content = await findByText(/body zero/i)
+  const content = await findByText(/this is a test/i)
   expect(content).toHaveClass("Content", "Theme-Test")
 
-  const footer = getByText(/2020/i)
+  const footer = getByText(/2020—\d{4}/i)
   expect(footer).toHaveClass("Footer")
 
   const book = container.firstChild
@@ -43,31 +45,34 @@ test("renders Book box", async () => {
 test("activates Book pages navigation buttons", async () => {
   const { findByText } = render(<Book className="Box Test" />)
 
+  expect(await findByText(/hello/i)).toBeInTheDocument()
+  expect(await findByText(/this is a test/i)).toBeInTheDocument()
+
   const next = await findByText(">")
   await act(async () => {
     next.click()
   })
-  expect(await findByText(/title one/i)).toBeInTheDocument()
-  expect(await findByText(/body one/i)).toBeInTheDocument()
+  expect(await findByText(/goodbye/i)).toBeInTheDocument()
+  expect(await findByText(/hope all went well/i)).toBeInTheDocument()
 
   const nextAgain = await findByText(">")
   await act(async () => {
     nextAgain.click()
   })
-  expect(await findByText(/title one/i)).toBeInTheDocument()
-  expect(await findByText(/body one/i)).toBeInTheDocument()
+  expect(await findByText(/goodbye/i)).toBeInTheDocument()
+  expect(await findByText(/hope all went well/i)).toBeInTheDocument()
 
   const previous = await findByText("<")
   await act(async () => {
     previous.click()
   })
-  expect(await findByText(/title zero/i)).toBeInTheDocument()
-  expect(await findByText(/body zero/i)).toBeInTheDocument()
+  expect(await findByText(/hello/i)).toBeInTheDocument()
+  expect(await findByText(/this is a test/i)).toBeInTheDocument()
 
   const previousAgain = await findByText("<")
   await act(async () => {
     previousAgain.click()
   })
-  expect(await findByText(/title zero/i)).toBeInTheDocument()
-  expect(await findByText(/body zero/i)).toBeInTheDocument()
+  expect(await findByText(/hello/i)).toBeInTheDocument()
+  expect(await findByText(/this is a test/i)).toBeInTheDocument()
 })
