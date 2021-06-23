288a31361d1f3a4fe4d4c5158a7f893065eeb3c3
Fernando Machado <lfmachado90@gmail.com>
2021-06-23 05:21:13 -0300

    adds welcome page

---
+++ src/book/00-welcome.js
@@ -0,0 +1,54 @@
+import React from "react"
+
+export const title = "CodeBook"
+export const body = (
+  <>
+    <h1>Olá!</h1>
+    <p>
+      Boas-vindas ao <b>CodeBook</b>, uma introdução a <b>HTML</b> e <b>CSS</b>!
+    </p>
+    <p>
+      Aqui você pode conhecer os princípios básicos das linguagens amplamente utilizadas para a construção das páginas
+      que você encontra na <b>internet</b> (como esse tutorial!)
+    </p>
+
+    <h2>Teoria + Prática</h2>
+    <p>A plataforma é dividida em três janelas:</p>
+
+    <table>
+      <tr>
+        <td>📚 material</td>
+        <td>⌨️ código</td>
+        <td>✨ resultado</td>
+      </tr>
+    </table>
+
+    <p>
+      Navegando nas páginas do <b>material</b>, você encontra explicações de conceitos fundamentais da web, acompanhados
+      de propostas de atividade, que podem ser imediatamente exercitadas na janela de <b>código</b> enquanto você
+      acompanha seu progresso, ao vivo, na janela de <b>resultado</b>.
+    </p>
+
+    <p>
+      Você pode navegar livremente pelas páginas sem perder seu trabalho, então não pense duas vezes antes de revisar
+      conteúdos e experimentar enquanto aprende!
+    </p>
+
+    <h2>Colabore</h2>
+    <p>Caso tenha interesse, você pode fazer parte desse projeto de diferentes frentes:</p>
+
+    <ul>
+      <li>revisando o material de ensino;</li>
+      <li>adicionando novas páginas ao material;</li>
+      <li>incluindo lições básicas de Javascript;</li>
+      <li>melhorando a usabilidade da plataforma;</li>
+      <li>refatorando o código-fonte da aplicação;</li>
+      <li>recomendando o CodeBook pra alguém;</li>
+      <li>contribuindo com alguma outra ideia!</li>
+    </ul>
+
+    <p>
+      Acesse o <a href="https://github.com/fernandomachado90/codebook">GitHub</a> do projeto e saiba como colaborar.
+    </p>
+  </>
+)
