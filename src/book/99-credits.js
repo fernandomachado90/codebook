cf70412700238923f1efac1b98fb655c0e5a4024
Fernando Machado <lfmachado90@gmail.com>
2021-06-23 07:14:20 -0300

    updates credits

--- src/book/99-credits.js
+++ src/book/99-credits.js
@@ -4,7 +4,28 @@ export const title = "Créditos"
 export const body = (
   <>
     <h1>VALEU!</h1>
-    <p>Esta é uma lista das pessoas que contribuíram com o projeto:</p>
+
+    <p>Que bom que chegou até aqui!</p>
+
+    <p>
+      Este guia de estudos foi originalmente desenvolvido entre 2018—2020, em mutirões de trabalho voluntário de pessoas
+      consultoras da{" "}
+      <a target="_blank" rel="noreferrer" href="https://www.thoughtworks.com/aceleradora">
+        ThoughtWorks Brasil
+      </a>{" "}
+      no âmbito da{" "}
+      <a target="_blank" rel="noreferrer" href="http://aceleradora-inclusiva.herokuapp.com/">
+        Aceleradora Inclusiva
+      </a>
+      , um projeto de educação popular em tecnologia viabilizado por uma parceria entre ThoughtWorks e PUC-RS.
+    </p>
+
+    <p>
+      Desde 2021, o material está disponibilizado no <b>CodeBook</b>, uma plataforma de ensino <i>open-source</i>, onde
+      se tornou aberto a contribuições da comunidade.
+    </p>
+
+    <p>Esta é uma lista das pessoas que contribuíram até agora:</p>
 
     <ul>
       <li>Fernando Machado</li>
