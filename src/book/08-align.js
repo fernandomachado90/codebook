2ad10080fe4b038888b6cdd7fd77d03770a081df
Fernando Machado <lfmachado90@gmail.com>
2021-06-26 03:57:13 -0300

    minor fix

--- src/book/08-align.js
+++ src/book/08-align.js
@@ -42,7 +42,7 @@ export const body = (
       item b<br />
       item c
     </p>
-    <p style={{ "text-align": "justify" }}>
+    <p style={{ "text-align": "justify", "font-style": "italic" }}>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
       magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
       consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
