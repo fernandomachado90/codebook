6e53fb1820f0c79f14fbcb47b1c6e7afef37c982
Fernando Machado <lfmachado90@gmail.com>
2021-06-29 16:26:21 -0300

    refactor footer

--- src/Book.js
+++ src/Book.js
@@ -51,8 +51,11 @@ function Book({ page = 0, className }) {
 }
 
 function Footer() {
+  const year = new Date().getFullYear()
+
   const [seconds, setSeconds] = useState(0)
   const formatTimestamp = (seconds) => new Date(seconds * 1000).toISOString().substr(11, 8)
+
   useEffect(() => {
     const interval = setInterval(() => {
       setSeconds((seconds) => seconds + 1)
@@ -60,15 +63,14 @@ function Footer() {
     return () => clearInterval(interval)
   }, [])
 
-  const year = new Date().getFullYear()
   return (
     <footer className="Footer">
       © Codebook
-      <br />2020—{year}
-      
+      <br />
+      2020—{year}
       <span>{formatTimestamp(seconds)}</span>
       <a className="Link" href={process.env.REACT_APP_CONTRIBUTING_URL} target="_blank" rel="noopener noreferrer">
-        Colabore <img alt="GitHub" src={`${process.env.PUBLIC_URL}/github.png`} /> 
+        Colabore <img alt="GitHub" src={`${process.env.PUBLIC_URL}/github.png`} />
       </a>
     </footer>
   )
