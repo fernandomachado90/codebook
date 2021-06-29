05544b4ac3e2d349f642b82b4f797a56131a8a9e
Fernando Machado <lfmachado90@gmail.com>
2021-06-29 15:40:43 -0300

    switches to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { BrowserRouter as Router, Switch, Route, useParams } from "react-router-dom"
+import { HashRouter as Router, Switch, Route, useParams } from "react-router-dom"
 import "./App.css"
 import Book from "./Book"
 import Code from "./Code"
