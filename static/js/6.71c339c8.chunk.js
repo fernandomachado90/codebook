0b1b70cd5b3ae60e5998f25b298051dd90ceff0e
Fernando Machado <lfmachado90@gmail.com>
2021-06-30 17:00:40 -0300

    Updates

---
+++ static/js/6.71c339c8.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpcodebook=this.webpackJsonpcodebook||[]).push([[6],{57:function(o,s,c){"use strict";c.r(s);c(518)}}]);
+//# sourceMappingURL=6.71c339c8.chunk.js.map
--- static/js/6.9713bc59.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpcodebook=this.webpackJsonpcodebook||[]).push([[6],{56:function(o,s,c){"use strict";c.r(s);c(518)}}]);
-//# sourceMappingURL=6.9713bc59.chunk.js.map
